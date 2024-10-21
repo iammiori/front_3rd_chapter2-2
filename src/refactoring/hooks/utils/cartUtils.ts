47b36b926a0a4e4792ba0d5d2a02fe215208699c
iammiori <immioriii@gmail.com>
2024-10-21 16:49:54 +0900

    refactor: make calculateItemTotal function to pure function

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,7 +1,19 @@
-import { CartItem, Coupon } from '../../../types';
+import { CartItem, Coupon, Discount } from '../../../types';
+
+const findDiscountRate = (discounts: Discount[], quantity: number) => {
+  return (
+    discounts.find((discount) => discount.quantity === quantity)?.rate ?? 0
+  );
+};
+
+const applyDiscount = (price: number, discountRate: number) => {
+  return price * (1 - discountRate);
+};
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const { product, quantity } = item;
+  const discountRate = findDiscountRate(product.discounts, quantity);
+  return applyDiscount(product.price, discountRate);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
