a594a935000187d4c2d4895eddcb236096296d7b
iammiori <immioriii@gmail.com>
2024-10-21 18:44:49 +0900

    refactor: refactor getMaxApplicableDiscount function

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,9 +1,23 @@
-import { CartItem, Coupon, Discount } from '../../../types';
+import { CartItem, Coupon } from '../../../types';
 
-const findDiscountRate = (discounts: Discount[], quantity: number) => {
-  return (
-    discounts.find((discount) => discount.quantity === quantity)?.rate ?? 0
+export const getMaxApplicableDiscount = (item: CartItem) => {
+  const {
+    product: { discounts },
+    quantity
+  } = item;
+  if (discounts.length === 0) {
+    return 0;
+  }
+
+  const applicableDiscounts = discounts.filter(
+    (discount) => discount.quantity === quantity
   );
+
+  if (applicableDiscounts.length === 0) {
+    return 0;
+  }
+
+  return discounts.reduce((maxRate, { rate }) => Math.max(maxRate, rate), 0);
 };
 
 const applyDiscount = (price: number, discountRate: number) => {
@@ -12,12 +26,8 @@ const applyDiscount = (price: number, discountRate: number) => {
 
 export const calculateItemTotal = (item: CartItem) => {
   const { product, quantity } = item;
-  const discountRate = findDiscountRate(product.discounts, quantity);
-  return applyDiscount(product.price, discountRate);
-};
-
-export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const discountRate = getMaxApplicableDiscount(item);
+  return applyDiscount(product.price * quantity, discountRate);
 };
 
 export const calculateCartTotal = (
