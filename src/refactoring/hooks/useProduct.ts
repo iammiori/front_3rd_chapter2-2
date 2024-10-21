24fd19198ad259dbaaa0374ad709bcbbfb3f1c11
iammiori <immioriii@gmail.com>
2024-10-21 16:29:47 +0900

    refactor: extract product logic into hooks

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,34 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>([...initialProducts]);
+
+  const updateProducts = (
+    products: Product[],
+    updateProduct: Product
+  ): Product[] => {
+    return products.map((product) =>
+      product.id === updateProduct.id ? updateProduct : product
+    );
+  };
+
+  const addProductToList = (
+    products: Product[],
+    newProduct: Product
+  ): Product[] => {
+    return [...products, newProduct];
+  };
+
+  const updateProduct = (updateProduct: Product) => {
+    setProducts((prev) => updateProducts(prev, updateProduct));
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prev) => addProductToList(prev, newProduct));
+  };
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined
+    products,
+    updateProduct,
+    addProduct
   };
 };
